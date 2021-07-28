a8aaa386a7c0d501552938ca01f9d10ba5d3b2da
duffytilleman <duffy.tilleman@gmail.com>
2021-07-28 08:01:51 -0700

    Update description in google-meet-space-to-toggle-mute

--- scripts/google-meet-space-to-toggle-mute.user.js
+++ scripts/google-meet-space-to-toggle-mute.user.js
@@ -2,7 +2,7 @@
 // @name         Google Meet - Space to toggle mute
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Toggle mute/unmute status with spacebar like slack does
+// @description  Toggle mute/unmute status with spacebar like zoom does
 // @author       duffy.tilleman@gmail.com
 // @match        https://meet.google.com/*
 // @icon         https://www.google.com/s2/favicons?domain=google.com
